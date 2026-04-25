466fbe3f7a9b0a9c3d72bf433d4c543c000fa154
kritikhatri <kritikakhatri014@gmail.com>
2026-04-25 18:22:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-import Row from "./components/Row";
-import "./index.css";
-
+import "./index.css"
 export default function App() {
   return (
-    <div className="bg-black min-h-screen">
+    <div>
       <Navbar />
-      <Banner />
-      <Row title="Trending Now" />
-      <Row title="Top Rated" />
-      <Row title="Action Movies" />
     </div>
   );
 }
